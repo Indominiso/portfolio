5ee4b6756bb64a5ce38faf71f90a6a05bf1467ef
RDimov <134715285+RDimov04@users.noreply.github.com>
2023-12-25 12:20:22 +0200

    a

--- src/components/Photos.tsx
+++ src/components/Photos.tsx
@@ -1,10 +1,8 @@
 function Photos() {
-  // This is just dummy data, replace it with your actual projects data
   const projects = [
     { name: "Project 1", description: "This is Project 1" },
     { name: "Project 2", description: "This is Project 2" },
     { name: "Project 3", description: "This is Project 3" },
-    // Add more projects as needed
   ];
 
   return (
