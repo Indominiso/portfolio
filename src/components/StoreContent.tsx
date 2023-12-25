7dde6e7d367e5cd6ace84ab2f0d01c3bc544abcd
RDimov <134715285+RDimov04@users.noreply.github.com>
2023-12-25 19:52:39 +0200

    wipstore

--- src/components/StoreContent.tsx
+++ src/components/StoreContent.tsx
@@ -1,18 +1,7 @@
 const StoreContent = () => {
   return (
     <div>
-      <h2>My Projects</h2>
-      <ul>
-        <li>
-          <h3>Project 1</h3>
-          <p>Description of Project 1</p>
-        </li>
-        <li>
-          <h3>Project 2</h3>
-          <p>Description of Project 2</p>
-        </li>
-        // Add more projects as needed
-      </ul>
+      <h2>WIP</h2>
     </div>
   );
 };
